0f6b64978c3b99b996a72a7c868893ebcd52f717
eugeniocachiombo <eugeniocachiombo@gmail.com>
2026-09-20 15:53:25 +0100

    perf: privatizar routas

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -32,41 +32,49 @@ const routes = [
         path: "anos-lectivos",
         name: "academic_year",
         component: () => import("@/views/AcademicYear/Index.vue"),
+        meta: { permition: 'academic_year.view' },
       },
       {
         path: "cursos",
         name: "course",
         component: () => import("@/views/Course/Index.vue"),
+        meta: { permition: 'course.view' },
       },
       {
         path: "classes",
         name: "class",
         component: () => import("@/views/Class/Index.vue"),
+        meta: { permition: 'class.view' },
       },
       {
         path: "disciplinas",
         name: "subject",
         component: () => import("@/views/Subject/Index.vue"),
+        meta: { permition: 'subject.view' },
       },
       {
         path: "Funções",
         name: "role",
         component: () => import("@/views/Role/Index.vue"),
+        meta: { permition: 'role.view' },
       },
       {
         path: "Permissões",
         name: "permition",
         component: () => import("@/views/Permition/Index.vue"),
+        meta: { permition: 'permition.view' },
       },
       {
         path: "funcões-usuario",
         name: "user_role",
         component: () => import("@/views/UserRole/Index.vue"),
+        meta: { permition: 'user_role.view' },
       },
       {
         path: "permissões-funcões",
         name: "role_permition",
         component: () => import("@/views/RolePermition/Index.vue"),
+        meta: { permition: 'role_permition.view' },
       },
     ],
   },
@@ -83,7 +91,7 @@ const router = createRouter({
 });
 
 // Middleware Global (Navigation Guard)
-router.beforeEach((to, _from, next) => {
+router.beforeEach(async (to, _from, next) => {
   const userStore = useUserStore();
 
   const token = localStorage.getItem("token");
@@ -113,6 +121,20 @@ router.beforeEach((to, _from, next) => {
   ) {
     return next({ name: "dashboard" });
   }
+
+
+  // ---- obs: posteriormente ser eliminado
+   if(isAuthenticated){
+
+      await userStore.initUser();
+      if (to.name != '/dashboard' && to.meta.permition) {
+        if ( !userStore.getPermition(to.meta.permition)) {
+          return next({name: 'dashboard'})
+        }
+      }
+
+    }
+    // ------
 
   next();
 });
